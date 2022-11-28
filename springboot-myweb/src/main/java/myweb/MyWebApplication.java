534a819a7639d6b9d52e830847c86385bd92f442
Kimjunseon <junseon0722@gmail.com>
2022-11-28 11:51:42 +0900

    myweb and component

---
+++ springboot-myweb/src/main/java/myweb/MyWebApplication.java
@@ -0,0 +1,25 @@
+package myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class MyWebApplication {
+	
+	@Controller
+	public class  HelloController{
+		@RequestMapping("/hello")
+		public String hello() {
+			return "/WEB-INF/views/hello.jsp";
+		}
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebApplication.class, args);
+		// try(ConfigurableApplicationContext ac = SpringApplication.run(MyWebApplication.class, args)){}
+		// 여기서 try구문은 try catch finally close 구문과 같음
+		// close가 발생되므로 web에서는 close가 되면 안됌
+	}
+}
