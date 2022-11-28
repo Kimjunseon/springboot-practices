40739ca56ab0056067cf3e661a774be186c32def
Kimjunseon <junseon0722@gmail.com>
2022-11-28 13:47:08 +0900

    수업내용

--- springboot-myweb/src/main/java/myweb/MyWebApplication.java
+++ springboot-myweb/src/main/java/myweb/MyWebApplication.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class MyWebApplication {
-	
 	@Controller
 	public class  HelloController{
 		@RequestMapping("/hello")
